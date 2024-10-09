dd3b0c7d72698c553546774c9657a105a37a53d9
Carine Croteau <carine.croteau@gmail.com>
2024-10-08 22:03:54 -0400

    Cours 3 - Input

--- src/app/Composants/hero/hero.component.ts
+++ src/app/Composants/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
-
+  @Input() nomHero = "";
 }
