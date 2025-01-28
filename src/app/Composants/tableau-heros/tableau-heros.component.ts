6f92b5f9a064b086939ee381c7be7b82138fd88a
Carine Croteau <carine.croteau@gmail.com>
2025-01-28 15:36:10 -0500

    tableau héros

--- src/app/Composants/tableau-heros/tableau-heros.component.ts
+++ src/app/Composants/tableau-heros/tableau-heros.component.ts
@@ -2,20 +2,16 @@ import { Component, ViewChild } from '@angular/core';
 import {MatTableDataSource, MatTable, MatTableModule} from '@angular/material/table';
 import {MatIconModule} from '@angular/material/icon';
 import { Hero } from '../../Interfaces/hero';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-
 import { HeroService } from '../../Services/hero.service';
-
-
 
 @Component({
   selector: 'app-tableau-heros',
   standalone: true,
-  imports: [MatTableModule, MatIconModule, MatPaginatorModule],
+  imports: [MatTableModule, MatIconModule],
   templateUrl: './tableau-heros.component.html',
   styleUrl: './tableau-heros.component.css'
 })
+
 export class TableauHerosComponent {
   dataSourceHeros: MatTableDataSource<Hero> = new MatTableDataSource();
   columnsToDisplay = ['nom'];
@@ -31,12 +27,10 @@ export class TableauHerosComponent {
   getHeros() { 
     this.heroService.getHeros().subscribe(
       resultat => {
-        console.log(resultat);
         this.dataSourceHeros = new MatTableDataSource(resultat);
         this.tableHeros.renderRows();
       }
     );
   }
-
 
 }
